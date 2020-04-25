88817a40a42ffea523d511f415ca714824e82968
fred-cook <fredcook789@gmail.com>
2020-04-25 18:20:41 +0100

    changed fonts in emacs and prettier config

--- doom/.doom.d/config.el
+++ doom/.doom.d/config.el
@@ -20,8 +20,8 @@
 ;; They all accept either a font-spec, font string ("Input Mono-12"), or xlfd
 ;; font string. You generally only need these two:
 ;;(setq doom-font (font-spec :name "Noto Sans Mono":size 13))
-(setq doom-font (font-spec :name "DroidSansMono Nerd Font Mono Book":size 16))
-;;( doom-font (font-spec:name "Source Code Pro Regular" : size 11))
+(setq doom-font (font-spec :name "DroidSansMono Nerd Font Mono":size 17))
+;;( doom-font (font-spec:name "Source Code Pro medium" : size 11))
 ;;(setq doom-font (font-spec :family "Fira Mono" :size 12))
 ;; There are two ways to load a theme. Both assume the theme is installed and
 ;; available. You can either set `doom-theme' or manually load a theme with the
@@ -84,23 +84,23 @@
 (require 'prettier-js)
 (add-hook 'js2-mode-hook 'prettier-js-mode)
 (setq prettier-js-args '(
-  {
-  "--arrowParens": "always",
-  "--bracketSpacing": true,
-  "--htmlWhitespaceSensitivity": "css",
-  "--insertPragma": false,
-  "--jsxBracketSameLine": false,
-  "--jsxSingleQuote": false,
-  "--proseWrap": "preserve",
-  "--quoteProps": "as-needed",
-  "--requirePragma": false,
-  "--semi": true,
-  "--singleQuote": false,
-  "--tabWidth": 4,
-  "--trailingComma": "es5",
-  "--useTabs": true,
-  "--vueIndentScriptAndStyle": false,
-  "--printWidth": 96
+{
+  "arrowParens": "always",
+  "bracketSpacing": true,
+  "htmlWhitespaceSensitivity": "css",
+  "insertPragma": false,
+  "jsxBracketSameLine": false,
+  "jsxSingleQuote": false,
+  "printWidth": 96,
+  "proseWrap": "preserve",
+  "quoteProps": "as-needed",
+  "requirePragma": false,
+  "semi": true,
+  "singleQuote": false,
+  "tabWidth": 4,
+  "trailingComma": "es5",
+  "useTabs": false,
+  "vueIndentScriptAndStyle": false
 }
 ))
 
